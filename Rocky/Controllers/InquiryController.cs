9ae281535b143150bce56019119caca42a172680
Vladislav <vladkorobov.work@gmail.com>
2023-04-01 16:03:03 +0200

    Third commit

--- Rocky/Controllers/InquiryController.cs
+++ Rocky/Controllers/InquiryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Rocky_DataAccess.Repostitory.IRepository;
+using Rocky_Models.ViewModels;
 using Rocky_Utility;
 
 namespace Rocky.Controllers
@@ -9,6 +10,9 @@ namespace Rocky.Controllers
     {
         private readonly IInquiryHeaderRepository _inqHRepo;
         private readonly IInquiryDetailRepository _inqDRepo;
+
+        [BindProperty] // Теперь при создании метода типа POST, все данные отсюда будут доступны для него
+        public InquiryVM InquiryVM { get; set; }
 
         public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
         {
@@ -20,5 +24,25 @@ namespace Rocky.Controllers
         {
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            InquiryVM = new InquiryVM()
+            {
+                inquiryHeader = _inqHRepo.FirstOrDefault(u => u.Id == id),
+                inquiryDetail = _inqDRepo.GetAll(u => u.InquiryHeaderId == id, includeProperties:"Product")
+            };
+
+            return View(InquiryVM);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList()
+        {
+            return Json(new { data = _inqHRepo.GetAll() });
+        }
+
+        #endregion
     }
 }
